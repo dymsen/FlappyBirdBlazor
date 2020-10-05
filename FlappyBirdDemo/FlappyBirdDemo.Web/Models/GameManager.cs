76518eb747fc5649ac8ec8fa0bcd9636e7493f2a
unknown <git.dymsen@pm.me>
2020-10-05 21:12:28 +0200

    Adding Bird Falling until the ground

---
+++ FlappyBirdDemo/FlappyBirdDemo.Web/Models/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlappyBirdDemo.Web.Models
+{
+    public class GameManager : INotifyPropertyChanged
+    {
+        private readonly int _gravity = 2;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BirdModel Bird { get; set; }
+        public bool IsRunning { get; set; } = false;
+        public GameManager()
+        {
+            Bird = new BirdModel();
+        }
+
+        public async void MainLoop()
+        {
+            IsRunning = true;
+            while (IsRunning)
+            {
+                Bird.Fall(_gravity);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Bird)));
+
+                if (Bird.DistanceFromGround <= 0)
+                {
+                    IsRunning = false;
+                }
+
+                await Task.Delay(20);
+            }
+        }
+    }
+}
--- FlappyBirdDemo/FlappyBirdDemo.Web/obj/Debug/netstandard2.1/Razor/App.razor.g.cs
+++ FlappyBirdDemo/FlappyBirdDemo.Web/obj/Debug/netstandard2.1/Razor/App.razor.g.cs
@@ -79,6 +79,13 @@ using FlappyBirdDemo.Web.Components;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 11 "D:\Programmieren\FlappyBirdBlazor\FlappyBirdDemo\FlappyBirdDemo.Web\_Imports.razor"
+using FlappyBirdDemo.Web.Models;
+
+#line default
+#line hidden
+#nullable disable
     public partial class App : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
